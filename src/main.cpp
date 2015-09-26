435a1eae68e5ce3b41eae509d82949acc55f333b
vyanor <vyanor@gmail.com>
2015-09-26 17:35:14 +0200

    remove headless

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include "ofMain.h"
 #include "ofApp.h"
-#include "ofAppNoWindow.h"
+//#include "ofAppNoWindow.h"
 
 //========================================================================
 int main(int argc, char *argv[] ){
 
 
-    ofAppBaseWindow* pWindow = new ofAppNoWindow();
+   // ofAppBaseWindow* pWindow = new ofAppNoWindow();
+    ofAppBaseWindow* pWindow = new ofAppGlutWindow();
 	ofSetLogLevel(OF_LOG_VERBOSE);
 	ofSetupOpenGL(pWindow,1280,720, OF_WINDOW);			// <-------- setup the GL context
 	ofApp *app = new ofApp();
